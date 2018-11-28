60e54d3d5c734c0b91af1a06e907c74bcaf7f6ed
manant96 <mananthakkar@yahoo.com>
2018-11-28 11:22:44 -0500

    Scanner_Parser
     Please enter the commit message for your changes. Lines starting

---
+++ src/SCANNER_PARSER/ScannerDemo.java
@@ -0,0 +1,29 @@
+package SCANNER_PARSER;
+
+
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "prog2.jay";
+	private static int counter = 1;																						
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+
+		System.out.println(file1);
+
+		while (!ts.isEndofFile()) {
+			System.out.print("Token " + counter + " : ");
+			Token t = ts.nextToken();
+			if(t == null) {
+				System.out.println("Null");
+			}
+			System.out.println(t.toString());
+			counter++;
+		}
+	}
+}
